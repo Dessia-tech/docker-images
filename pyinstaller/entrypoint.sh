b4fd32c5fe3b6657f3165de1cc57f5d9b754cf0b
Steven Masfaraud <masfaraud@dessia.tech>
2022-10-07 10:40:04 +0200

    feat: dessia pyinstaller

---
+++ pyinstaller/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Allow the workdir to be set using an env var.
+# Useful for CI pipiles which use docker for their build steps
+# and don't allow that much flexibility to mount volumes
+WORKDIR=${SRCDIR:-/src}
+
+
+if [ -f requirements.txt ]; then
+    pip install -r requirements.txt
+fi # [ -f requirements.txt ]
+
+echo "$@"
+
+if [[ "$@" == "" ]]; then
+    pyinstaller --clean -y --dist ./dist/linux --workpath /tmp *.spec
+    chown -R --reference=. ./dist/linux
+else
+    sh -c "$@"
+fi # [[ "$@" == "" ]]
