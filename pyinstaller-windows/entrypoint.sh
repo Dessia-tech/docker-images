cc47b074261108f9ed3b003b9c1ff035d5643628
Baltha Wilson <balthazar.wilson@etu.utc.fr>
2022-12-05 14:12:06 +0100

    fix: build for windows

--- pyinstaller-windows/entrypoint.sh
+++ pyinstaller-windows/entrypoint.sh
@@ -6,20 +6,20 @@ set -e
 # Useful for CI pipiles which use docker for their build steps
 # and don't allow that much flexibility to mount volumes
 
-WORKDIR=/src
+WORKDIR=${SRCDIR:-/src}
 
 cd $WORKDIR
 
 pwd
 
 if [ -f requirements.txt ]; then
-    $PYTHON -m pip install -r requirements.txt
+    wine $PYTHON -m pip install -r requirements.txt
 fi # [ -f requirements.txt ]
 
 echo "$@"
 
 if [[ "$@" == "" ]]; then
-    $PYINSTALLER --clean -y --dist ./dist/windows --workpath /tmp *.spec
+    wine $PYINSTALLER --clean -y --dist ./dist/windows --workpath /tmp *.spec
     chown -R --reference=. ./dist/windows
 else
     sh -c "$@"
